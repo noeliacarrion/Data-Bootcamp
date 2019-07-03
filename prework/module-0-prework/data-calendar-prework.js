d37f9439398ab16a042e3bd80fca4c62c0fb411a
YonatanRA <yeho@mail.com>
2019-07-03 22:20:02 +0200

    lessons

---
+++ prework/module-0-prework/data-calendar-prework.js
@@ -0,0 +1,44 @@
+const Prework = [
+{
+  lessons: [
+    {
+      topic: 'prepareDevEnv',
+      learning_units: [
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/command-line-basics.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/challenges/homebrew.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/challenges/git.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/challenges/python.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/challenges/mysql.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/challenges/jupyter.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/introduction-to-git.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/00-prepare-dev-env/github-project.md'}
+      ]
+    },
+    {
+      topic: 'pythonBeginner',
+      learning_units: [
+        {type: 'lesson',     url: '/lessons/module-0-prework/01-python-beginner/python-basics.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/01-python-beginner/data-structures.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/01-python-beginner/control-flow.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/01-python-beginner/files.md'}
+      ]
+    },
+    {
+      topic: 'mySQLbeginner',
+      learning_units: [
+        {type: 'lesson',     url: '/lessons/module-0-prework/02-mysql-beginner/mysql-basics.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/02-mysql-beginner/summary-stats.md'}
+      ]
+    },
+    {
+      topic: 'probabilityStatistics',
+      learning_units: [
+        {type: 'lesson',     url: '/lessons/module-0-prework/03-probability-statistics/descriptive-stats.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/03-probability-statistics/distributions-sampling.md'},
+        {type: 'lesson',     url: '/lessons/module-0-prework/03-probability-statistics/correlation-regression.md'}
+      ]
+    },
+]
+}];
+
+module.exports = Prework;
