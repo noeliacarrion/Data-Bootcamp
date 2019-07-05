01d07294fbb6357057f17cb0c99457ca7627aecc
YonatanRA <yeho@mail.com>
2019-07-05 14:01:07 +0200

    pipeline

--- Misc/pipeline/main.py
+++ Misc/pipeline/main.py
@@ -1,19 +1,19 @@
 # main.py
 
 
-import pipeline
-#from pipeline import *
+#import pipeline
+from pipeline import *
 
 
 if __name__=='__main__':
-	#print (ranking()[0])
-	#print (rating()[0])
-	#print (reviews()[0])
-	#print (cost()[0])
+	print (ranking()[0])
+	print (rating()[0])
+	print (reviews()[0])
+	print (cost()[0])
 	#print (summary()[0])
 	
-	#print (return_results()[0])
-	print (pipeline.return_results()[0])
+	print (return_results()[0])
+	#print (pipeline.return_results()[0])
 
 
 
